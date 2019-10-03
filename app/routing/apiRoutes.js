86840edaffb7fc091f2c104dcfb62636184df0d9
Carmen SanDiego <efemar82@gmail.com>
2019-10-03 00:13:48 -0700

    fixed ids for buttons

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,7 @@ app.get("/api/friends", function (req, res) {
     res.json(table);
 });
 
-// Create New Reservations - takes in JSON input
+// Capturing data from survey
 app.post("/survey", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
@@ -16,4 +16,23 @@ app.post("/survey", function (req, res) {
 
 });
 
+//Capturing data from survey questions
+$("#makeres").on("click", function(event) {
+  event.preventDefault();
+  var newReservation = {
+    id: $("#newid").val().trim(),
+    name: $("#name").val().trim(),
+    email: $("#email").val().trim(),
+    phone: $("#phone").val().trim()
+  };
 
+//Saving data to friends.js file, performing match and displaying results
+  $.post("/make", newReservation)
+    .then(function(data) {
+      console.log("add.html", data);  
+      if (data === "Added to the Waiting List") {
+        alert("Sorry...You have been added to the Waiting List");
+      }
+      else {alert("Adding new reservation...")}
+    });
+});
